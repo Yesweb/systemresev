780897aafe851d1ebc4a0dcb7537046c0d14bf6b
Wiyoso Aji <aji_gondronk@yahoo.com>
2014-04-05 19:27:08 +0700

    Auth
    Hal akses user checkin dan checkout sudah dipisah

--- design/default/block_design_check.php
+++ design/default/block_design_check.php
@@ -1,12 +1,25 @@
 <?php
+$usern = $_SESSION['username'];
+$stat = $db->Execute("SELECT permit FROM user WHERE username='$usern'");
+while($data_termstat = $stat->FetchRow()) {
+	$cek_termstat = $data_termstat['permit'];
+}
 ?>
 <center>
-<h4>Checkin/Chekout</h4>
+<?php
+if ($cek_termstat == 1) {
+	echo "<h4>Checkin</h4>";
+} else if ($cek_termstat == 2) {
+	echo "<h4>Chekout</h4>";
+} else {
+	echo "<h4>Checkin/Chekout</h4>";
+}
+?>
 <form action="" method="get">
 	<input type="hidden" name="view" value="input">
 	<input type="text" id="focus" name="memberid" value="">
 	<input type="hidden" name="term" value="900">
-	<input type="hidden" name="termstat" value="3">
+	<input type="hidden" name="termstat" value="<?=$cek_termstat?>">
 	<input class="submitbutton" type="submit" value="Submit">
 </form>
 </center>
