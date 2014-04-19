72e4ed8c813b9d51f5eced6b6bf6b55793a1a222
Wiyoso Aji <aji_gondronk@yahoo.com>
2014-04-19 23:54:36 +0700

    Filter

--- design/default/block_design_filterform.php
+++ design/default/block_design_filterform.php
@@ -1,27 +1,41 @@
 <form action="index.php?view=filres" name="filter" method="post">
-	<label for="idmember">ID MMember</label>
-	<select name="idmember">
-		<option value=""></option>
-		<option value="100000000001">100000000001</option>
-		<option value="100000000002">100000000002</option>
-	</select>
+	<label for="idmember">ID Member</label>
+	<input type="text" name="idmember" value="">
 	<label for="tanggal">Tanggal</label>
 	<select name="tanggal">
 		<option value=""></option>
-		<option value="2014-04-18">2014-04-18</option>
-		<option value="2014-04-17">2014-04-17</option>
+<?php
+	$tgl = $db->Execute("SELECT DISTINCT DATE_FORMAT(`check`, '%Y-%m-%d') AS tgl FROM `checkin` ORDER BY tgl ASC");
+	while($data_tgl = $tgl->FetchRow()) {
+?>
+		<option value="<?=$data_tgl['tgl']?>"><?=$data_tgl['tgl']?></option>
+<?php
+	}// EOF while($data_tgl = $tgl->FetchRow())
+?>
 	</select>
 	<label for="status">Status</label>
 	<select name="status">
 		<option value=""></option>
-		<option value="1">Masuk</option>
-		<option value="2">Keluar</option>
+<?php
+	$status = $db->Execute("SELECT * FROM status");
+	while($data_status = $status->FetchRow()) {
+?>
+		<option value="<?=$data_status['status']?>"><?=$data_status['name']?></option>
+<?php
+	}// EOF while($data_status = $status->FetchRow())
+?>
 	</select>
 	<label for="author">Author</label>
 	<select name="author">
 		<option value=""></option>
-		<option value="admin">Admin</option>
-		<option value="keluar">keluar</option>
+<?php
+	$author = $db->Execute("SELECT DISTINCT author FROM checkin");
+	while($data_author = $author->FetchRow()) {
+?>
+		<option value="<?=$data_author['author']?>"><?=$data_author['author']?></option>
+<?php
+	}// EOF while($data_author = $author->FetchRow())
+?>
 	</select>
 	<button type="submit">Search</button>
 </form>
